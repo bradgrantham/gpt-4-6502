fc386c937224c4e28eb15df551f73db1d2760ecf
Brad Grantham <grantham@plunk.org>
2023-03-20 22:34:49 -0700

    factor out common OpenAI code from tools

--- run-all-instructions.py
+++ run-all-instructions.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python3
 
 import sys
-import os
-import openai
-import time
-import random
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-with open("prompt.txt", "r") as prompts:
-    system_content = prompts.readline()
-    preamble = prompts.read()
+import runprompt
 
 # Fizzbuzz assembled in decimal
 program = {
@@ -75,89 +67,6 @@ opcode_to_description_strings = {
     208: "BNE %(M)d; %(BYTES)d bytes; if Z flag is clear, also add the signed value to PC",
     76: "JMP %(JMP)d; %(BYTES)d bytes; set the PC to the value of the next two instruction bytes, %(P)d + %(Q)d * 256",
 }
-
-# From https://platform.openai.com/docs/guides/rate-limits/error-mitigation
-# define a retry decorator
-def retry_with_exponential_backoff(
-    func,
-    initial_delay: float = 1,
-    exponential_base: float = 2,
-    jitter: bool = True,
-    max_retries: int = 10,
-    rate_errors: tuple = (openai.error.RateLimitError,),
-    retry_errors: tuple = (openai.error.APIConnectionError,openai.error.Timeout,),
-):
-    """Retry a function with exponential backoff."""
-
-    def wrapper(*args, **kwargs):
-        # Initialize variables
-        num_retries = 0
-        delay = initial_delay
-
-        # Loop until a successful response or max_retries is hit or an exception is raised
-        while True:
-            try:
-                return func(*args, **kwargs)
-
-            # Retry on specific errors
-            except rate_errors as e:
-                # Increment retries
-                num_retries += 1
-
-                # Check if max retries has been reached
-                if num_retries > max_retries:
-                    raise Exception(
-                        f"Maximum number of retries ({max_retries}) exceeded."
-                    )
-
-                # Increment the delay
-                delay *= exponential_base * (1 + jitter * random.random())
-
-                # Sleep for the delay
-                time.sleep(delay)
-
-	    # Brad added this after receiving many APIConnectionError
-	    # and Timeout during the initial weeks of gpt-4
-            except retry_errors as e:
-                # Increment retries
-                num_retries += 1
-
-                # Check if max retries has been reached
-                if num_retries > max_retries:
-                    raise Exception(
-                        f"Maximum number of retries ({max_retries}) exceeded."
-                    )
-
-                # Sleep for the delay
-                time.sleep(10.0)
-
-            # Raise exceptions for any errors not specified
-            except Exception as e:
-                raise e
-
-    return wrapper
-
-@retry_with_exponential_backoff
-def GetChatGPTResult(state_string, insn_bytes_string):
-    prompt = '''%s
-
-Set state to %s
-%s
-''' % (preamble, state_string, insn_bytes_string)
-
-    # print("%s" % prompt)
-    # sys.exit(0)
-
-    completion = openai.ChatCompletion.create(
-      model="gpt-4",
-      temperature=0.0,
-      messages=[
-          {"role": "system", "content": system_content},
-          {"role": "user", "content": prompt},
-        ]
-    )
-
-    return completion.choices[0].message
 
 # run our 6502 through one instruction
 def execute6502(PC, bytes, A, X, N, Z, MEMORY):
@@ -238,7 +147,7 @@ query_only_after = 0
 # Only run this many instructions.
 # To run a short test, set to e.g. 10.
 # There are 288 instructions, so set to e.g. 300 to run all of them.
-only_execute = 300     
+only_execute = 300
 
 go = True       # if False, exit main loop
 while go and only_execute > 0:
@@ -276,7 +185,7 @@ while go and only_execute > 0:
 
     if query_only_after <= 0:
         # send to GPT and print that result
-        response = GetChatGPTResult(state_string, insn_bytes_string)
+        response = runprompt.GetChatGPTResult(state_string, insn_bytes_string)
 
         print("Chat-GPT:")
         lines = response["content"].split("\n")
--- test-some.py
+++ test-some.py
@@ -1,31 +1,7 @@
 import sys
 import os
-import openai
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-with open("prompt.txt", "r") as prompts:
-    system_content = prompts.readline()
-    preamble = prompts.read()
-
-def GetChatGPTResult(state_string, insn_bytes_string):
-    prompt = '''%s
-
-Set state to %s
-%s''' % (preamble, state_string, insn_bytes_string)
-
-    # print("%s" % prompt)
-    # sys.exit(0)
-
-    completion = openai.ChatCompletion.create(
-      model="gpt-4",
-      messages=[
-          {"role": "system", "content": system_content},
-          {"role": "user", "content": prompt},
-        ]
-    )
-
-    return completion.choices[0].message
+import runprompt
 
 input = sys.stdin.read()
 lines = input.split("\n")
@@ -36,5 +12,5 @@ while len(lines) > 0:
     del(lines[0:2])
     print(state)
     print(insns)
-    result = GetChatGPTResult(state, insns)
+    result = runprompt.GetChatGPTResult(state, insns)
     print(result["content"])
